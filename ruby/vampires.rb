3db7e23f88022332e7bb61e92d0bc6c9c348d8f5
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-10 15:55:58 -0400

    Create questions for vampire work survey

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+year = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurace = gets.chomp
