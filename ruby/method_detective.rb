8f3ef71748f8e0dd10af63bb53952fd451b6f7ba
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-15 18:07:10 -0400

    Complete all the cases for method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -16,24 +16,25 @@ puts "enhance".center(17)
 puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".insert(9, ' suspects')
 #=> "the usual suspects"
 
 puts " suspects".prepend("the usual")
+puts " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
 puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+puts "The mystery of the missing first letter".delete("T")
 # => "he mystery of the missing first letter"
 
 puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+puts "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
