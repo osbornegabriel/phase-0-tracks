6b1d09a89ed160527034956385c966f581f5ccf9
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-15 22:46:09 -0400

    create designer.rb and test hashes within it

---
+++ ruby/designer.rb
@@ -0,0 +1,27 @@
+
+job_application = {
+	"name" =>" ",
+	"home" =>"The Sea",
+	"email" => " ",
+	"phone" => " ",
+	"fave_blue" => " ",
+
+	pasiley: false,
+	chevrons: false,
+	photorealistic_woodsy_scene: false,
+	abstract_woodsy_scene: false,
+	ombre: nil,
+}
+
+job_application[:hired] = "yes"
+
+job_application['name'] = "Jeevers"
+# job_application[:pasiley] = true
+# job_application[:ombre] = "Fierce"
+
+name_home = job_application['name']
+p job_application[name_home]
+
+
+# andrew = 2+2
+# p job_application
