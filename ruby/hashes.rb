fd079e40380d31049021131086099ef574bb1a63
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-16 15:12:23 -0400

    Create correction key for client errors, print results

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,5 @@
 #Create hash with the keys that will be used to fill it
-client_info = {
+Client_info = {
 	name: nil,
 	age: nil,
 	kids: nil,
@@ -14,26 +14,26 @@ puts "Welcome to the designer care client collection data deluge.
 Here are some questions we'd like you to answer about this client."
 puts "What is your client's name?"
 name = gets.chomp
-client_info[:name] = name
+Client_info[:name] = name
 
 puts "What is your client's age? (Please use integer instead of phrases like 'on death's door'.)"
 age = gets.chomp.to_i
-client_info[:age] = age
+Client_info[:age] = age
 
 puts "How many children does your client have?"
 kids = gets.chomp.to_i
-client_info[:kids] = kids
+Client_info[:kids] = kids
 
 puts "What type of decoration theme does your client prefer?"
 decor_theme = gets.chomp
-client_info[:decor_theme] = decor_theme
+Client_info[:decor_theme] = decor_theme
 
 puts "What quirky preferences does the client have? (If none, type 'nil')"
 quirks = gets.chomp
 	if quirks == "nil"
 		quirks = nil
 	end
-client_info[:quirks] = quirks
+Client_info[:quirks] = quirks
 
 puts "True or False: Your client knows that decoraction is ultimately meaningless,
 as soon the dark one shall rise and bathe in the blood of the innocent,
@@ -48,16 +48,35 @@ meaningless = gets.chomp.downcase
 	else
 		meaningless = nil
 	end
-client_info[:meaningless] = meaningless
+Client_info[:meaningless] = meaningless
+
+#This is a module for changing a value in one of the keys for Client_info
+def key_change(key)
+	puts "what is new value you wish to input for #{key}"
+	new_input = gets.chomp
+	Client_info[key] = new_input
+end
 
 
 #Display hash to user and ask if information is correct
+correct = "n"
 while correct != "y"
-puts "Here is your client info: #{client_info}"
+puts "Here is your client info: #{Client_info}"
 puts "Is all of this info correct?(y/n)"
 correct = gets.chomp
-		if correct == "n"
-			puts "Thank you very much! Enjoy the rest of your day."
-		elsif == "n"
-			puts "Which key do you wish to change?"
-			
+	if correct == "y"
+		puts "Thank you very much! Enjoy the rest of your day."
+	elsif correct == "n"
+		puts "Which key do you wish to change?"
+		puts "Options: name, age, kids, decor_theme, quirks, meaningless (Otherwise type 'cancel')"
+		change = gets.chomp.to_sym
+		if change == :cancel
+			correct = "y"
+		else
+		  key_change(change)
+		end
+	end
+end
+
+puts "Here's a copy of your completed form"
+p Client_info
