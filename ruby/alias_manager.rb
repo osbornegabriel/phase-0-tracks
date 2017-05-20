10f503fd33d99a819eb0bd2edb728bedea9981c8
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-20 16:30:22 -0400

    build vowel converter in alias_manager

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,3 +5,21 @@ full_name = gets.chomp
 #Reverse first and last names
 alias_name = full_name.split(' ').reverse!.join(' ')
 p alias_name
+
+alias_letters = alias_name.split('')
+
+def vowel(vowel)
+	list_vowels = "aeioua"
+	if list_vowels.include? vowel
+		vowel_value = list_vowels.index(vowel)
+		p vowel_value
+		vowel_value += 1
+		vowel = list_vowels[vowel_value]
+	end
+end
+
+alias_letters.map! do |letter|
+	vowel(letter)
+end
+
+p alias_letters
