184ef5b50bb0a7304ec04821e0237cd26773202c
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-22 13:33:00 -0400

    Talk about puppies, adorable

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+SPECIES ---------------
+Puppy
+
+CHARACTERISTICS -------------
+Legs: Four
+Hair: Yes
+Personality: Super adorbs
+Friendly: Usually
+Ears: Two
+Noise: Barks
+
+BEHAVIOUR-----------------
+Jump
+Bark
+Chase
+Smile
+=end
