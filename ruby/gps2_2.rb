b5a01b6a2dc39aaa042047bf554bb9cb1c62951a
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-22 21:39:40 -0400

    pseudocode method for creating list and method for adding item to list

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,36 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+	#Create blank hash entitled grocery_list
+	#single variable for parameter in method
+	#Use .split, place items into array
+	#Use .each method to place hash, set value to 1 for each item
+	#yield?
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+	# 3 variables for method - (list, item_name, quantity=0)
+	# list[item_name] = quantity
+# output: updated hash
+
+# Method to remove an item from the list
+# input: 
+# steps:
+# output: updated hash
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input: hash
+# steps: use .each method
+# output: puts "You have #{value} many #{key}"
