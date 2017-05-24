23c0017db68a3a6426306781fc7e9c7ce6bb3ee3
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-24 17:07:27 -0400

    Create three setter methods for santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,35 @@
 class Santa
-	def speak
-		puts "Ho, ho, ho! Haaaappy holidays!"
+	def initialize(gender_string, ethnicity_string)
+		puts "Initializing Santa instance here"
+		@gender = gender_string
+		@ethnicity = ethnicity_string
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
+			 "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
+	def celebrate_birthday
+		@age += 1
 	end
 
 	def eat_milk_and_cookies(cookie)
 		print "That was a good #{cookie}!"
 	end
 
-	def initialize(gender_string, ethnicity_string)
-		puts "Initializing Santa instance here"
-		@gender = gender_string
-		@ethnicity = ethnicity_string
-		# @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
-		# 	 "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+	def gender_bender=(new_gender_string)
+		@gender = new_gender_string
 	end
+
+	def get_mad_at(reindeer_name_string)
+		@reindeer_ranking.delete(reindeer_name_string)
+		@reindeer_ranking.push(reindeer_name_string)
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+
 end
 
 # Jolly = Santa.new
@@ -33,26 +48,34 @@ end
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
-# santas = []
-# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-# example_genders.length.times do |i|
-#   santas << Santa.new(example_genders[i], example_ethnicities[i])
-# end
-
 santas = []
-santas_hash = {
-	"agender" => "black",
-	"female" => "Latino",
-	"bigender" => "white",
-	"male" => "Japanese-African",
-	"female" => "prefer not to say",
-	"gender fluid" => "Mystical Creature (unicorn)",
-	"N/A" => "N/A"
-}
-
-santas_hash.each do |gender, ethnicity|
-	santas << Santa.new(gender, ethnicity)
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
 end
 
-p santas
+#This approach did not totally work, because there are two keys named "female"
+# santas = []
+# santas_hash = {
+# 	"agender" => "black",
+# 	"female" => "Latino",
+# 	"bigender" => "white",
+# 	"male" => "Japanese-African",
+# 	"female" => "prefer not to say",
+# 	"gender fluid" => "Mystical Creature (unicorn)",
+# 	"N/A" => "N/A"
+# }
+
+# santas_hash.each do |gender, ethnicity|
+# 	santas << Santa.new(gender, ethnicity)
+# end
+
+santas[1].celebrate_birthday
+p santas[1]
+
+santas[0].get_mad_at("Comet")
+p santas[0]
+
+santas[-1].gender_bender=("Not Your Business")
+p santas[-1]
