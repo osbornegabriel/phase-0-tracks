6bce641d65229385cfc8d5db68634f297c5acfe6
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-24 16:32:09 -0400

    Add santa intialization method to santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -11,15 +11,48 @@ class Santa
 		puts "Initializing Santa instance here"
 		@gender = gender_string
 		@ethnicity = ethnicity_string
-		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
-			 "Comet", "Cupid", "Donner", "Blitzen"]
+		# @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
+		# 	 "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = 0
 	end
 end
 
-Jolly = Santa.new
+# Jolly = Santa.new
 
 
 
-Jolly.speak
-Jolly.eat_milk_and_cookies("oatmeal cookie")
+# Jolly.speak
+# Jolly.eat_milk_and_cookies("oatmeal cookie")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+santas = []
+santas_hash = {
+	"agender" => "black",
+	"female" => "Latino",
+	"bigender" => "white",
+	"male" => "Japanese-African",
+	"female" => "prefer not to say",
+	"gender fluid" => "Mystical Creature (unicorn)",
+	"N/A" => "N/A"
+}
+
+santas_hash.each do |gender, ethnicity|
+	santas << Santa.new(gender, ethnicity)
+end
+
+p santas
