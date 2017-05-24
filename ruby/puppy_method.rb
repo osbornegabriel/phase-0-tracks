ca393310a6f76e0b7767e25f3b4f5e05cb443c4d
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-23 22:23:01 -0400

    create 50 instances of No_fun class

--- ruby/puppy_method.rb
+++ ruby/puppy_method.rb
@@ -1,41 +1,41 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
- def speak(number)
-  	number.times do puts "Woof!"
-  	end 
-  end
+#  def speak(number)
+#   	number.times do puts "Woof!"
+#   	end 
+#   end
 
-  def rolls_over
-  	puts "Rolls over!"
-  end 
+#   def rolls_over
+#   	puts "Rolls over!"
+#   end 
 
-  def dog_years(age)
-  	dog_age = age/7
-  	return dog_age
-  end 
+#   def dog_years(age)
+#   	dog_age = age/7
+#   	return dog_age
+#   end 
 
-  def stubborn
-  	p nil 
-  end 	
+#   def stubborn
+#   	p nil 
+#   end 	
 
-  def initialiez 
-  	print "Initializing a new puppy instance..."
-  end 	
+#   def initialiez 
+#   	print "Initializing a new puppy instance..."
+#   end 	
 
-end
+# end
 
-cookie = Puppy.new
+# cookie = Puppy.new
 
-cookie.fetch("ball")
-cookie.speak(5)
-cookie.rolls_over
-p cookie.dog_years(5)
-cookie.stubborn 
+# cookie.fetch("ball")
+# cookie.speak(5)
+# cookie.rolls_over
+# p cookie.dog_years(5)
+# cookie.stubborn 
 #cookie.initialiez
 
 
@@ -52,4 +52,8 @@ class No_fun
   def no_lunch_break
     puts "Vending snack"
   end
+end
+
+50.times do
+  No_fun.new
 end
