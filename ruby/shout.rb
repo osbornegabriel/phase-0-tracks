cfb4b82294f7851c64d016e421c47f23440cb937
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-25 16:35:58 -0400

    Make new mix-in version of shout module, add Belligerent_drunk and Church_lady classes to shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + " Yah-tah!!! :D :D :D"
+# 	end
+# end
+
+# p Shout.yell_angrily("Green Beans")
+# p Shout.yell_happily("Broccoli")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
-		words + " Yah-tah!!! :D :D :D"
+	def yell_happily(words)
+		words + "! Eugepae!!! :D :D :D"
 	end
 end
 
-p Shout.yell_angrily("Green Beans")
-p Shout.yell_happily("Broccoli")
+class Belligerent_drunk
+	include Shout
+end
+
+class Church_lady
+	include Shout
+end
+
+belligerent_drunk = Belligerent_drunk.new
+p belligerent_drunk.yell_happily("Beer? For Me?")
+
+church_lady = Church_lady.new
+p church_lady.yell_angrily("Have you no shame")
