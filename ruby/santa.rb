f9b2a54cbd8de0bd8313700d5e4018a2dc1f8eaa
Gabriel J. Osborne <osbornegabriel@gmail.com>
2017-05-24 22:25:25 -0400

    Create random santa generator in santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,13 +2,16 @@ class Santa
 	attr_reader :age, :ethnicity
 	attr_accessor :gender
 
-	def initialize(gender_string, ethnicity_string)
+	def initialize(gender_string, ethnicity_string, age = 0)
 		puts "Initializing Santa instance here"
 		@gender = gender_string
 		@ethnicity = ethnicity_string
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen",
 			 "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = age
+		p @gender
+		p @ethnicity
+		p @age
 	end
 
 	# def age
@@ -59,12 +62,12 @@ end
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
 
 #This approach did not totally work, because there are two keys named "female"
 # santas = []
@@ -82,14 +85,23 @@ end
 # 	santas << Santa.new(gender, ethnicity)
 # end
 
-santas[1].celebrate_birthday
-p santas[1]
+# santas[1].celebrate_birthday
+# p santas[1]
 
-santas[0].get_mad_at("Comet")
-p santas[0]
+# santas[0].get_mad_at("Comet")
+# p santas[0]
 
-santas[-1].gender = "Not Your Business"
-p santas[-1]
+# santas[-1].gender = "Not Your Business"
+# p santas[-1]
 
-p santas[2].age
-p santas[2].ethnicity
+# p santas[2].age
+# p santas[2].ethnicity
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+age_generator = Random.new
+75.times do
+  Santa.new(example_genders.sample, example_ethnicities.sample, age_generator.rand(140))
+end
