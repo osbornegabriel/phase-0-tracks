4b3cb0ab050d49bcb17dc4bf0991050f36ea2391
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-27 15:34:24 -0400

    Create guess method in word_game.rb

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -5,7 +5,7 @@ class Word_game
   def initialize(answer)
     @answer = answer
     @answer_array = answer.split(//)
-    @guess = answer.length.times {|i| "_" * i}
+    @guess = answer.length.times {|i| "_ " * i}
     end
     if answer.length < 4
       @guess_limit = 6
@@ -16,5 +16,22 @@ class Word_game
     end
   end
 
+  def guess
+    if guess != answer && guess_limit != 0
+      letter_guess = ""
+      while letter_guess.length != 1
+      puts "What letter do you wish to guess?"
+      letter_guess = gets.chomp.downcase
+      end
+      answer_comparison(letter_guess)
+    end
+
+    if @guess == @answer
+      puts "Great job!!! You guessed that the word is #{@answer}!"
+    elsif @guess != @answer && @guess_limit == 0
+      puts "Wow, you really suck at this. You made #{@guess_limit} guesses and still couldn't solve this. Go sit in the corner and think about what you've done!"
+    else
+      puts "You're not there yet, keep guessing!"
+    end
 
 end
