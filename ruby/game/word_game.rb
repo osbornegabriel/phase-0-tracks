eeac522e47c76a4380645417436cd53a7382dc69
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-27 16:17:39 -0400

    More code cleanup on word_game.rb

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,5 +1,5 @@
 class Word_game
-  attr_reader :answer, :answer_array, :guess_limit
+  attr_reader :answer, :guess_limit
   attr_accessor :guess
 
   def initialize(answer)
@@ -26,7 +26,7 @@ class Word_game
 
 
   def guess
-    if guess != answer && guess_limit != 0
+    if @guess != @answer && @guess_limit != 0
       letter_guess = ""
       while letter_guess.length != 1
       puts "What letter do you wish to guess?"
@@ -38,7 +38,7 @@ class Word_game
     if @guess == @answer
       puts "Great job!!! You guessed that the word is #{@answer}!"
     elsif @guess != @answer && @guess_limit == 0
-      puts "Wow, you really suck at this. You made #{@guess_limit} guesses and still couldn't solve this. Go sit in the corner and think about what you've done!"
+      puts "Wow, you really suck at this. You couldn't guess the word if you had 26 guesses. Go sit in the corner and think about what you've done!"
     else
       puts "You're not there yet, keep guessing!"
     end
