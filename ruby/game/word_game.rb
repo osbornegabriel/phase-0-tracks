cfd337567fcf656ebd7db7f6d9f02be0f63adeb5
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-27 15:03:11 -0400

    Add guess conversion into ___ and attr to Word_game class.

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -1,12 +1,18 @@
 class Word_game
+  attr_reader: :answer :answer_array :guess_limit
+  attr_accessor: :guess
+
   def initialize(answer)
     @answer = answer
+    @answer_array = answer.split(//)
+    @guess = answer.length.times {|i| "_" * i}
+    end
     if answer.length < 4
-      @guess = 6
+      @guess_limit = 6
     elsif answer.length > 6
-      @guess = 10
+      @guess_limit = 10
     else
-      @guess = 8
+      @guess_limit = 8
     end
   end
 
