c95c81762bf41f31eebb5c9de124ef442faebf56
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-31 15:21:37 -0500

    Create test.js, build js reverse function in explore.js

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+//.reverse method for js
+//assign "hello" to var hello
+//make empty var reverse_hello
+//for each letter of "hello" :
+//reverse_hello = reverse_hello + letter
+//(optional) make hello = reverse_hello
