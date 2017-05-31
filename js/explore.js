bd130169e9dff3aace4a2b569bde9c9e3ab1138b
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-31 15:58:38 -0500

    build reverse function in js from pseudocode

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,19 @@
 //.reverse method for js
 //assign "hello" to var hello
-//make empty var reverse_hello
+//make var reverse_hello = ""
 //for each letter of "hello" :
-//reverse_hello = reverse_hello + letter
+//Take the last index value in word
+//reverse_hello = reverse_hello + last_letter
 //(optional) make hello = reverse_hello
+
+function reverse(word) {
+  var newWord = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
+    }
+  console.log(newWord);
+}
+
+
+
+reverse("jazz")
