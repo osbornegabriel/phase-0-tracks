057c545c3744d407668a7d3fa76bb715169de9c5
Gabriel Osborne <osbornegabriel@gmail.com>
2017-06-01 12:06:29 -0500

    Create object maker that takes two arrays as input

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,5 +4,18 @@ var horseNames = ["Sparkle Mane", "Mildew", "Rainshine", "Mug"];
 colors.push("Sunset Blue");
 horseNames.push("Ed");
 
-console.log(colors);
-console.log(horseNames);
+// console.log(colors);
+// console.log(horseNames);
+
+function objectMaker(array1, array2) {
+  var newObject = {};
+  if (array1.length == array2.length){
+    // New object, horseNames = keys, colors = values
+    for (var i = array1.length - 1; i >= 0; i--){
+      newObject[array1[i]] = array2[i];
+    }
+  }
+  return newObject
+}
+
+console.log(objectMaker(horseNames, colors));
