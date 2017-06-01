b1a52b8b8aa08124a30ecabec93eb2db2df5643c
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-31 23:23:21 -0500

    Write pseudo-code for longestPhrase function in algos.js

---
+++ js/algos.js
@@ -0,0 +1,5 @@
+// Write a function that returns longest phrase in array
+// var = ""
+// build loop that iterates through entire array, and provides index-counter
+// if array[indexCounter].length is > var.length
+// var = array[indexCounter]
