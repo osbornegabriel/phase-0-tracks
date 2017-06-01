77c4578fb24ead0e488b9e7915c1270364e5ef95
Gabriel Osborne <osbornegabriel@gmail.com>
2017-06-01 12:29:57 -0500

    Create Constructor Function for object 'car'

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,24 +1,36 @@
-var colors = ["red", "blue", "green", "cucumber"];
-var horseNames = ["Sparkle Mane", "Mildew", "Rainshine", "Mug"];
+// var colors = ["red", "blue", "green", "cucumber"];
+// var horseNames = ["Sparkle Mane", "Mildew", "Rainshine", "Mug"];
 
-colors.push("Sunset Blue");
-// horseNames.push("Ed");
+// colors.push("Sunset Blue");
+// // horseNames.push("Ed");
 
-// console.log(colors);
-// console.log(horseNames);
+// // console.log(colors);
+// // console.log(horseNames);
 
-function objectMaker(array1, array2) {
-  var newObject = {};
-  if (array1.length == array2.length){
-    // New object, horseNames = keys, colors = values
-    for (var i = array1.length - 1; i >= 0; i--){
-      newObject[array1[i]] = array2[i];
-    }
-    return newObject;
-  }
-  else {
-    return "These arrays are not the same size";
-  }
+// function objectMaker(array1, array2) {
+//   var newObject = {};
+//   if (array1.length == array2.length){
+//     // New object, horseNames = keys, colors = values
+//     for (var i = array1.length - 1; i >= 0; i--){
+//       newObject[array1[i]] = array2[i];
+//     }
+//     return newObject;
+//   }
+//   else {
+//     return "These arrays are not the same size";
+//   }
+// }
+
+// console.log(objectMaker(horseNames, colors));
+
+
+
+function Car(color, age, isConvertible){
+  this.color = color;
+  this.age = age;
+  this.isConvertible = isConvertible
 }
 
-console.log(objectMaker(horseNames, colors));
+var newCar = new Car("red", "5", "true")
+
+console.log(newCar)
