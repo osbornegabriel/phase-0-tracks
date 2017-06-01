f0912e819c6ace3dd6fe4ad867cad534aadebdce
Gabriel Osborne <osbornegabriel@gmail.com>
2017-05-31 23:31:45 -0500

    Write pseucode for hash comparison function

--- js/algos.js
+++ js/algos.js
@@ -3,3 +3,12 @@
 // build loop that iterates through entire array, and provides index-counter
 // if array[indexCounter].length is > var.length
 // var = array[indexCounter]
+
+
+
+
+// n algos.js, write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+//matchMade = false
+//hashOne.each |:key, value|
+//if hashTwo[:key] == value
+// then matchMade = true
