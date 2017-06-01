bea19bdd0b5652ff4c2ecede4703a5fd9c79a0dc
Gabriel Osborne <osbornegabriel@gmail.com>
2017-06-01 11:30:51 -0500

    Create colors and horseNames arrays, add one value to each array, print arrays to check work

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["red", "blue", "green", "cucumber"];
+var horseNames = ["Sparkle Mane", "Mildew", "Rainshine", "Mug"];
+
+colors.push("Sunset Blue");
+horseNames.push("Ed");
+
+console.log(colors);
+console.log(horseNames);
